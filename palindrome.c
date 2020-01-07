5085f045b8ea06d927a7f1a1d66ff5a38cd70df8
i34 <moroccanhashish@gmail.com>
2020-01-07 03:58:01 +0000

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -6,9 +6,10 @@ int main(int argc, char *argv[])
 {
 	if(argc < 2) exit(EXIT_FAILURE);
 
-	char *lineptr = NULL;
-	ssize_t ssize, lpos, pal, len;
-	size_t n = 0;
+	size_t n;
+	ssize_t ssize, len, lpos;
+	int lchar, rchar;
+	char *lineptr=NULL;
 	FILE *stream;
 
 	stream = fopen(argv[1], "r");
@@ -16,18 +17,17 @@ int main(int argc, char *argv[])
 
 	while((ssize = getline(&lineptr, &n, stream)) != -1){
 		if(ssize < 4) continue;
-		len=ssize-1;
-		lineptr[len]=0;
 
-		pal=1;
-		for(lpos=0; lpos<(len/2); ++lpos){
-			if(tolower(lineptr[lpos]) != tolower(lineptr[len-lpos-1])){
-				pal=0;
-				break;
-			}
+		lpos=0, len=ssize-1, lineptr[len]=0;
+
+		while(len > lpos){
+			lchar=tolower(lineptr[lpos++]);
+			rchar=tolower(lineptr[--len]);
+			if(lchar != rchar) goto end;
 		}
 
-		if(pal) printf("%s\n", lineptr);
+		printf("%s\n", lineptr);
+		end: continue;
 	}
 
 	fclose(stream);
